1922461dcda58d7e313b6f0c10bf77aebee2475a
Daniel DiCocco <dicoccod@bc.edu>
2020-11-06 13:46:48 -0500

    changes

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 
 d3.json('airports.json', d3.autoType).then(data=>{ 
     
-    console.log('data', data); // simply call the update function with the supplied data
+    console.log('data', data);
     
-    update(data, type, sort);
+    var margin = {top: 50, right: 50, bottom: 50, left: 50};
+    var width = 600 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom
 
-    d3.select(".sort-button").on("click", () =>{
-        sort = !sort;
-        update(data, type, sort);
-    });
+    const svg = d3.select(".chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.select("#group-by").on("change", (e) => {
-            type = e.target.value;
-            update(data, type, sort);
-    });
 
+    
   
 
 })
